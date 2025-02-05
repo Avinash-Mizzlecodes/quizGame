44f3c61d498eeb695ad1d7b4bf7a70d15ac89e3f
Reactnative-App <er.yog.trip@gmail.com>
2025-02-05 15:37:11 +0530

    commit herouku setup.

--- server.js
+++ server.js
@@ -1,20 +1,34 @@
-require('dotenv').config();
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const mongodb = require('./config/db');
+import 'dotenv/config';
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import path from 'path';
+import mongodb from './config/db.js'; // Ensure file extension is included
+
+import userRoutes from './routes/userRoutes.js';
+
 const app = express();
 
+// Connect to MongoDB
 mongodb();
+
 // Middleware
-app.use(express.json()); // Parse JSON requests
-app.use(cors()); // Enable CORS
-app.use(express.static('public')); // Serve static files (for frontend)
+app.use(express.json());
+app.use(cors());
+app.use(express.static('public'));
 
-
-// Import routes
-const userRoutes = require('./routes/userRoutes');
+// Routes
 app.use('/user', userRoutes);
+
+// Deploying for Production
+if (process.env.NODE_ENV === 'production') {
+    const dirPath = path.resolve();
+    app.use(express.static(path.join(dirPath, 'public')));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(dirPath, 'index.html'));
+    });
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
